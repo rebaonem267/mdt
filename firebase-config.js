d411a45e50afcc9c1933440a74697bea637e9c7b
rebaonem267 <rebaonem.267@gmail.com>
2026-09-08 12:22:19 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -72,7 +72,7 @@ no re-uploading of anything.
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDEUwIaOor4AZQx7d-OzydVHu3G_sXKwRM",
   authDomain: "mophato.firebaseapp.com",
   projectId: "mophato",
