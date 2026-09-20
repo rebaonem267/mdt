eb89feeeb8ae5092c460206a2457abff97c2ba34
rebaonem267 <rebaonem.267@gmail.com>
2026-09-20 23:58:49 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -255,7 +255,7 @@ used by browser applications.
 
 ========================================================= */
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For_Firebase JS SDK v7.20.0 and later, measurementId is optional
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDEUwIaOor4AZQx7d-OzydVHu3G_sXKwRM",
   authDomain: "mophato.firebaseapp.com",
