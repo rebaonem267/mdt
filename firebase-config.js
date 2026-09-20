c520d2d78c3ff150994d9d3185c070b0fb284f57
rebaonem267 <rebaonem.267@gmail.com>
2026-09-20 23:23:44 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -255,20 +255,15 @@ used by browser applications.
 
 ========================================================= */
 
-const FIREBASE_CONFIG = {
-
-  apiKey: "YOUR_API_KEY",
-
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-  projectId: "YOUR_PROJECT_ID",
-
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-
-  appId: "YOUR_APP_ID"
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDEUwIaOor4AZQx7d-OzydVHu3G_sXKwRM",
+  authDomain: "mophato.firebaseapp.com",
+  projectId: "mophato",
+  storageBucket: "mophato.firebasestorage.app",
+  messagingSenderId: "1093871152499",
+  appId: "1:1093871152499:web:3ea5f9c3ff72936fc12906",
+  measurementId: "G-70LCNKTG03"
 };
 
 
